5054d02dc01de624a9d32c72ae8fc636c157cae3
Giacomo Biancalana <biancalanagiacomo20@gmail.com>
2023-06-12 00:10:15 +0200

    axios, async, ts-node etc..

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("kmskw");
+import axios from 'axios';
+
+const token: string = "6005659200:AAFtTvmf6yc1DetLobvGfsHrO7MgEDAA6o0";
+const url: string = "https://api.telegram.org/bot"
+const method: string = "/getMe"
+
+async function sendTo() {
+  let pp: any;
+  try {
+    pp = await axios.get(`${url}${token}${method}`);    
+  } catch (error) {
+    console.error(error);
+  }
+  return pp.data;
+}
+
+(async ()=>console.log(await sendTo()))();
