75b7f9f189366af056c061fbf0d270172511649b
Giacomo Biancalana <biancalanagiacomo20@gmail.com>
2024-09-03 14:54:19 +0200

    Webhook (done in Ngrok), express, todos

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,49 @@
-import axios from 'axios';
-
 import * as dotenv from 'dotenv';
 dotenv.config()
 
-const token: string = process.env.TOKEN;
-const url: string = "https://api.telegram.org/bot";
-const method: string = "/getMe";
+import axios from 'axios';
+import express from 'express';
+// import bodyParser from 'body-parser';
 
-async function sendTo() {
-  let pp: any;
-  try {
-    pp = await axios.get(`${url}${token}${method}`);    
-  } catch (error) {
-    console.error(error);
-  }
-  return pp.data;
-}
+const TOKEN: string = process.env.TOKEN;
+const URL =  "https://api.telegram.org/bot";
+const URL_TOKEN = `${URL}${TOKEN}`;
 
-(async ()=>console.log(await sendTo()))();
+const app = express();
+app.use(express.json());  // FON-DA-MEN-TA-LE  //TODO: da capire
+//TODO: //app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log('Example app listening to port 3000 !!!');
+})
+
+app.post('/webhook', async (req, res) => {
+  console.log(req.body);
+  
+  const chatId = req.body.message.chat.id;
+  const text = req.body.message.text;
+  await axios.post(`${URL_TOKEN}/sendMessage`, {
+    chat_id: chatId,
+    text,
+  });
+  
+  return res.send();
+});
+
+// TODO: git config
+// TODO:set commands
+// TODO:cloudfare workers
+
+// async function sendTo() {
+//   let res: any;
+//   try {
+//     res = await axios.get(`${URL}${TOKEN}${method}`);
+//     // res = await axios.get('https://jsonplaceholder.typicode.com/todos/3');
+//   } catch (error) {
+//     console.error(error);
+//   }
+//   return res.data; 
+// }
+
+// (async ()=>console.log(await sendTo()))();
 // guarda video https://www.youtube.com/watch?v=IlsygSzikOQ per togliere roba async
