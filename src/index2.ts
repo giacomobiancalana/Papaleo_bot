e6f6990d9c9e4c420e9cbf32b97a893167d29e39
Giacomo Biancalana <biancalanagiacomo20@gmail.com>
2024-09-28 12:47:33 +0200

    Formattazione

--- src/index2.ts
+++ src/index2.ts
@@ -1,13 +1,15 @@
 import * as dotenv from 'dotenv';
 dotenv.config()
+
 //TODO: facciamolo senza pacchetti npm (per cloudfare workers).
 //TODO: poi proviamo con Wrangler CLI e pacchetti npm.
+
 import axios from 'axios';
 import express from 'express';
 
 const TOKEN = process.env.TOKEN;
 const PORT: number = parseInt(process.env.PORT) || 3000;
-const URL =  "https://api.telegram.org/bot";
+const URL = "https://api.telegram.org/bot";
 const URL_TOKEN = `${URL}${TOKEN}`;
 
 const app = express();
@@ -33,14 +35,14 @@ app.post('/webhook', async (req, res) => {
   //TODO: da aggiungere il token nell'endpoint (/webhook/<TOKEN>) per renderlo unico,
   // così chi non conosce il token non può chiamare l'api
   console.log(req.body);
-  
+
   const chatId = req.body.message.chat.id;
   const text = req.body.message.text;
   await axios.post(`${URL_TOKEN}/sendMessage`, {
     chat_id: chatId,
     text,
   });
-  
+
   return res.send();
 });
 
